7c15c60af7f8fdf4f6158e13905533c1e0da40f1
evan999 <emeshberge@gmail.com>
2020-05-12 00:12:05 -0400

    Rebuild of TransactionBatch microservice with JDBC API.

---
+++ src/test/java/com/financetracker/TransactionBatch/TransactionBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financetracker.TransactionBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
